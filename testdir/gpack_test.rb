5628a4b7958cf9810aa2900eb7c8c81e5b8ad518
Aaron Cook <acook@allegromicro.com>
2018-02-13 15:52:14 -0500

    Cleanup

--- testdir/gpack_test.rb
+++ testdir/gpack_test.rb
@@ -1,20 +1,3 @@
-#!/bin/env ruby
+#!/bin/bash
 
-current_file = File.dirname(__FILE__)
-
-lib_path = current_file+"/../lib"
-
-begin
-  require 'byebug'
-rescue LoadError
-  # Don't crash if debug gem isn't there
-end
-
-load lib_path+"/headers.rb"
-load lib_path+"/gpack_readme.rb"
-load lib_path+"/GitReference.rb"
-load lib_path+"/GitCollection.rb"
-load lib_path+"/parallel.rb"
-load lib_path+"/parse_repos.rb"
-load lib_path+"/ssh.rb"
-load lib_path+"/gpack.rb"
+ruby -I../lib ../bin/gpack  "$@"
